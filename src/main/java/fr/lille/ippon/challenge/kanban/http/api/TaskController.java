ec066d40b45bb6b3be5a8ef401dd51969a2afb34
Cédric MAGNE <cmagne@ippon.fr>
2023-01-06 18:17:21 +0100

    Implement tasks REST API

--- src/main/java/fr/lille/ippon/challenge/kanban/http/api/TaskController.java
+++ src/main/java/fr/lille/ippon/challenge/kanban/http/api/TaskController.java
@@ -3,11 +3,14 @@ package fr.lille.ippon.challenge.kanban.http.api;
 import fr.lille.ippon.challenge.kanban.domain.Task;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
+@RequestMapping("/tasks")
 public class TaskController {
 
+    @GetMapping
     public List<Task> getTasks() {
         // TODO faire fonctionner. Refactorer ?
         return List.of(new Task("my first task"));
